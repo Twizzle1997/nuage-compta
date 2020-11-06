2f2f3aa07e7790b11aaf4df935c8b37e67fa3ef8
Utilisateur <72632400+tomcdev63@users.noreply.github.com>
2020-11-06 15:41:52 +0100

    import auto

--- import_auto.py
+++ import_auto.py
@@ -1,20 +1,23 @@
+print("toto")
+
+
+
 import os
 import shutil
 
 
-import os
 
-CATEGORIES = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "Y", "V", "W", "X", "Y", "Z","z_coeur"]
+CATEGORIES = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "Y", "V", "W", "X", "Y", "Z"]
 
 if not os.path.exists("data_set_reduit"):
-    os.mkdir("data_set_reduit")
-    for letter in CATEGORIES:
-        i = 0
-        if not os.path.exists(f"data_set_reduit/{letter}"):
-            os.mkdir(f"data_set_reduit/{letter}")
-            for file in os.listdir(f"data/alphabet-dataset/{letter}/"):
-                shutil.copy(file, f"data_set_reduit/{letter}")
-                i += 1
-                if i == 200:
-                    break
+    os.makedirs("data_set_reduit")
+for letter in CATEGORIES:
+    i = 0
+    if not os.path.exists(f"data_set_reduit/{letter}"):
+        os.makedirs(f"data_set_reduit/{letter}")
+        for file in os.listdir(f"data/alphabet-dataset/{letter}/"):
+            shutil.copy(f"data/alphabet-dataset/{letter}/{file}", f"data_set_reduit/{letter}")
+            i += 1
+            if i == 200:
+                break
 
